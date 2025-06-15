fbe205ebb4cb23958946d73e9abea76231c53a29
thet982 <prooomaster79@gmail.com>
2025-06-14 20:42:39 -0700

    cambios leonardo

--- WinFormsApp6/WinFormsApp6/obj/Debug/net8.0-windows/WinFormsApp6.AssemblyInfo.cs
+++ WinFormsApp6/WinFormsApp6/obj/Debug/net8.0-windows/WinFormsApp6.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp6")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d20176f074050392ae898d26e7968e6efdab4d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34b01a0d69530c41feed64ab5e609f05f3329c87")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp6")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp6")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
